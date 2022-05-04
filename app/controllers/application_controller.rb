25fb82c06e74d6687d5a8522980eb58ac4e9129b
Allison <amh03160@gmail.com>
2022-05-04 12:50:51 -0500

    commit tree 42c9dc3764ad55c75223891eeef07d8db0a4f84c

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+rescue_from ActiveRecord::RecordInvalid, with: :error_msg
+
+def summary
+    movie = Movie.find(params[:id])
+    render json: movie, serializer: MovieSummarySerializer
+end
+
+private
+    def error_msg(invalid)
+        render json: {errors:invalid.errors}
+    end
+
 end
